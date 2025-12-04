b2c9988184cc12371cb3aed6b8a292ed54e8add3
Yogendra Bisht <bishtyogendra96436372@gmail.com>
2025-12-04 23:30:52 +0530

    password section created

--- first/app/otp/page.js
+++ first/app/otp/page.js
@@ -32,7 +32,7 @@ const page = () => {
   return (
     <div className='flex items-center justify-center bg-purple-900 h-dvh w-dvw'>
       <div className='flex items-center flex-col justify-evenly p-3 h-[250px] w-[300px] bg-black rounded-2xl text-white gap-4'>
-        <output className='border-white border-2 rounded p-1.5 text-2xl h-[45px] w-[150px] text-center'>{Otp}</output>
+        <output className='border-green-600 border-2 rounded p-1.5 text-2xl h-[45px] w-[150px] text-center'>{Otp}</output>
         <button className='text-xl rounded-2xl bg-blue-400 p-2.5 hover:bg-green-700 active:text-sm' onClick={(e)=>{
           showOtp(e);
         }}>Generate another</button>
--- first/app/password/page.js
+++ first/app/password/page.js
@@ -1,8 +1,35 @@
+"use client"
 import React from 'react'
+import { useState } from "react";
 
 const page = () => {
+
+  const [Password, setPassword] = useState('')
+  function showPassword(e){
+    let alphabets ="abcdefghijklmnopqrstuvwxyz0123456789@#$&";
+    let password ="";
+    for (let index = 0; index < 8; index++) {
+      let point = Math.ceil(Math.random()*100)
+      point = point%40;
+      let char = alphabets.charAt(point);
+      password = password+char;
+      
+    }
+    setPassword(password);
+  }
   return (
-    <div className='flex items-center justify-center bg-green-500 h-dvh w-dvw'> password page</div>
+    <div className='flex items-center justify-center bg-green-500 h-dvh w-dvw'> 
+    <div className='flex items-center flex-col justify-evenly p-3 h-[250px] w-[300px] bg-black rounded-2xl text-white gap-4'>
+        <output className='border-red-500 border-2 rounded p-1.5 text-2xl h-[45px] w-[150px] text-center'>{Password}</output>
+        <button className='text-xl rounded-2xl bg-blue-400 p-2.5 hover:bg-green-700 active:text-sm' onClick={(e)=>{
+          showPassword(e);
+        }}>Generate Password</button>
+
+        
+        
+
+      </div>
+    </div>
   )
 }
 
